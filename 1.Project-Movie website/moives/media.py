ab66a75bbfbb156a4c9703e6a482eeda4728ae18
liulinzhu2018 <40175844+liulinzhu2018@users.noreply.github.com>
2018-07-15 16:51:58 +0800

    Update media.py

--- 1.Project-Movie website/moives/media.py
+++ 1.Project-Movie website/moives/media.py
@@ -2,9 +2,26 @@ import webbrowser
 
 
 class Movie():
-    """movie doc"""
+    """ Movie class is used to save movies' information and show trailer."""
     VAILD_RATINGS = ['G', 'PG', 'PG-13', 'R']
 
+    """ construct function
+
+        Prameters
+        ---------
+        arg1 : str
+            movie title
+        arg2 : str
+            movie storyline
+        arg3 : str
+            poster image
+        arg4 : str
+            trailer youtobe url
+
+        Returns
+        -------
+        none
+    """
     def __init__(
                 self,
                 movie_title,
@@ -16,5 +33,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    """show trailer"""
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
